8af3fd0d8d53e86f555250eadfb01d510d89db64
duong <hoangthaiduong@gmail.com>
2022-07-01 22:32:08 -0600

    Fixed the config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ ny = 2160
 type_bytes = 4 # float322 = 4 bytes
 dfx = [nx  , nx  , nx, nx*3, nx*3] # x dimension of the 5 faces
 dfy = [ny*3, ny*3, ny, ny  , ny  ] # y dimension of the 5 faces
-idx2_exe = 'idx2.exe'
+idx2_exe = './idx2'
