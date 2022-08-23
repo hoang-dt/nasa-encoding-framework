979e8cd32efe834ce780703aa046e1f016aa34ed
duong <hoangthaiduong@gmail.com>
2022-08-23 13:28:42 -0600

    Added more comments for aashish

--- convert_to_idx2.py
+++ convert_to_idx2.py
@@ -12,7 +12,7 @@ def convert_to_idx2(raw_file, dataset_name, long_field_name, dimensions):
     + ' --dims ' + repr(dimensions[0]) + ' ' + repr(dimensions[1]) + ' ' + repr(dimensions[2]) + ' '
     + ' --accuracy 1e-7 '
     + ' --num_levels 4 ' # TODO: move to input param
-    + ' --brick_size 64 64 32 ' # TODO: move to input param
+    + ' --brick_size 64 64 32 ' # TODO: move to input param #aashish: maybe either do 32 32 32 or 64 64 64
     + ' --bricks_per_tile 512 '
     + ' --tiles_per_file 4096 '
     + ' --files_per_dir 4096 '
