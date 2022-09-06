443fa547b80a022f68a5c9c2bbf40107d803de57
duong <hoangthaiduong@gmail.com>
2022-09-06 08:52:41 -0600

    Update the conversion script

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
 # Parameters
-n_faces = 5 # [0, 1, 2, 3, 4] # aashish: change to [0, 1, 2, 3, 4, 5]
-n_depths = 90 # aashish: change to 52
-nx = 2160 # aashish: 1440
-ny = 2160 # aashish: 1440
-type_bytes = 4 # float322 = 4 bytes
-dfx = [nx  , nx  , nx, nx*3, nx*3] # x dimension of the 5 faces # aashish: change to nx, nx, nx, nx, nx, nx
-dfy = [ny*3, ny*3, ny, ny  , ny  ] # y dimension of the 5 faces # aashish: change to ny, ny, ny, ny, ny, ny
-idx2_exe = './idx2'
+n_square_faces = 13
+n_faces        = 5 # [0, 1, 2, 3, 4]
+n_depths       = 90
+nx             = 2160
+ny             = 2160
+type_bytes     = 4 # float322 = 4 bytes
+dfx            = [nx  , nx  , nx, nx*3, nx*3] # x dimension of the 5 faces
+dfy            = [ny*3, ny*3, ny, ny  , ny  ] # y dimension of the 5 faces
+input_file     = 'files-u.txt'
+dataset_name   = 'llc2160'
+field_name     = 'u'
+time_block     = 1024
+time_range     = (0, 0) # set to (0, 0) to encode all time steps, otherwise set to (begin, end) to encode time steps begin..end-1
+depth_rang     = (0, 0)
+face_range     = (0, 0)
+idx2_exe       = './idx2App'
+n_processes    = 8
--- convert_llc.py
+++ convert_llc.py
@@ -1,19 +1,40 @@
 # Put all the time steps for a certain face (at a certain depth) into a single volume
-# To run this file: python convert_llc.py files-v.txt llc2160 v 1024
-# Every combination of (face, depth, 1024 time steps) goes into one idx2 file
+# To run this file, provide the config file: python convert_llc.py config.py
 
 import glob
-import mmap
 import numpy as np
 import os
 import shutil
 import sys
 from time import sleep
-from config import n_faces, n_depths, nx, ny, type_bytes, dfx, dfy
-import config
-from convert_to_idx2 import *
 from multiprocessing import Process
 
+# import the config file
+if len(sys.argv) != 2:
+  print('need 1 argument: the config file')
+  exit()
+config = __import__(sys.argv[1])
+
+#from config import n_faces, n_depths, nx, ny, type_bytes, dfx, dfy
+
+def convert_to_idx2(raw_file, dataset_name, long_field_name, dimensions):
+  command = (
+    config.idx2_exe + ' --encode '
+    + ' --input ' + raw_file + ' '
+    + ' --name ' + dataset_name + ' '
+    + ' --field ' + long_field_name + ' '
+    + ' --type float32 '
+    + ' --dims ' + repr(dimensions[0]) + ' ' + repr(dimensions[1]) + ' ' + repr(dimensions[2]) + ' '
+    + ' --accuracy 1e-7 '
+    + ' --num_levels 4 ' # TODO: move to input param
+    + ' --brick_size 32 32 32 ' # TODO: move to input param #aashish: maybe either do 32 32 32 or 64 64 64
+    + ' --bricks_per_tile 512 '
+    + ' --tiles_per_file 4096 '
+    + ' --files_per_dir 4096 '
+    + ' --out_dir .')
+  print(command)
+  os.system(command)
+  os.remove(raw_file)
 
 # Form the name of the .raw output file from a list of parameters
 def form_output_file_name(field_name, time_from, time_to, face, depth):
@@ -22,7 +43,7 @@ def form_output_file_name(field_name, time_from, time_to, face, depth):
     + 'face-' + repr(face)
     + '-depth-' + repr(depth)
     + '-time-' + repr(time_from) + '-' + repr(time_to))
-  dimensions = (dfx[face], dfy[face], time_to-time_from)
+  dimensions = (config.dfx[face], config.dfy[face], time_to-time_from)
   output_file = long_field_name
   return output_file, long_field_name, dimensions
 
@@ -33,20 +54,20 @@ def form_output_file_name(field_name, time_from, time_to, face, depth):
 # level_0_face_0_time_0.raw
 # level_0_face_1_time_0.raw
 #....
-# merge level_i_face_j_time_0.raw ... level_i_face_j_time_1023.raw -> into a single raw file for every i,j 
+# merge level_i_face_j_time_0.raw ... level_i_face_j_time_1023.raw -> into a single raw file for every i,j
 def extract_face_across_time(files, time_from, time_to, face, depth, output_file):
   # For each time step, skip to the face of interest and copy out the bytes
   print(output_file)
   with open(output_file, 'w+b') as g:
-    skip_bytes = depth * nx * ny * 13 * type_bytes # aashish: change 13 to 6
+    skip_bytes = depth * config.nx * config.ny * config.n_square_faces * config.type_bytes
     for f in range(0, face):
-      skip_bytes += dfx[f] * dfy[f] * type_bytes
-    face_bytes = type_bytes * dfx[face] * dfy[face]
+      skip_bytes += config.dfx[f] * config.dfy[f] * config.type_bytes
+    face_bytes = config.type_bytes * config.dfx[face] * config.dfy[face]
     for t in range(time_from, time_to):
       with open(files[t], "rb") as f:
         f.seek(skip_bytes)
         buf = f.read(face_bytes)
-        dt = np.dtype('>f')        
+        dt = np.dtype('>f')
         np_array_be = np.frombuffer(buf, dtype=dt)
         np_array_le = np_array_be.byteswap() # aashish: do not do the byteswap (to convert big endian to little endian)
         g.write(np_array_le)
@@ -54,49 +75,46 @@ def extract_face_across_time(files, time_from, time_to, face, depth, output_file
 
 
 if __name__ == '__main__':
-  # Main program
-  # --------------------------------------------------------------
-  if len(sys.argv) != 5:
-    print('need 4 arguments: input_file dataset_name field_name time_block')
-    print('  input_file is a file that contains a list of files each storing one time step')
-    print('  dataset_name is the name of the dataset (no space)')
-    print('  field_name is the name of the field (no space)')
-    print('  time_block indicates the number of time steps that will be grouped in one idx2 file')
-    print('example: python convert_llc.py files.txt llc2160 u 64')
-    exit()
-
-  input_file    = sys.argv[1]
-  dataset_name  = sys.argv[2]
-  field_name    = sys.argv[3]
-  time_block    = int(sys.argv[4])
-
   files = []
-  with open(input_file) as s:
+  with open(config.input_file) as s:
     files = [f.strip('\n') for f in s]
-
   n_time_steps = len(files)
 
-
   # For each time block, we first write a raw file, then convert it to idx2
-  for t in range(0, n_time_steps, time_block):
+  time_begin = config.time_range[0]
+  time_end   = config.time_range[1]
+  if time_begin == time_end:
+    time_begin = 0
+    time_end = n_time_steps
+  face_begin = config.face_range[0]
+  face_end = config.face_range[1]
+  if face_begin == face_end:
+    face_begin = 0
+    face_end = config.n_faces
+  depth_begin = config.depth_range[0]
+  depth_end = config.depth_range[1]
+  if depth_begin == depth_end:
+    depth_begin = 0
+    depth_end = config.n_depths
+  for t in range(time_begin, time_end, config.time_block):
     t_from = t
-    t_to = min(t + time_block, n_time_steps)
+    t_to = min(t + config.time_block, n_time_steps)
     #print(t, time_block, t_to)
-    for d in range(0, n_depths):
-      for f in range(0, n_faces):
+    for d in range(depth_begin, depth_end):
+      for f in range(face_begin, face_end):
         # check if the file has been converted and stored
-        output_file, long_field_name, dimensions = form_output_file_name(field_name, t_from, t_to, f, d)
-        idx2_file_name = dataset_name + '/' + output_file + '.idx2'
+        output_file, long_field_name, dimensions = form_output_file_name(config.field_name, t_from, t_to, f, d)
+        idx2_file_name = config.dataset_name + '/' + output_file + '.idx2'
         print(idx2_file_name)
         if os.path.exists(idx2_file_name):
           print('found')
           continue
-        dir_name = dataset_name + '/' + output_file
+        dir_name = config.dataset_name + '/' + output_file
         if os.path.exists(dir_name):
-          shutil.rmtree(dataset_name + '/' + output_file) # remove the dir first to avoid writing a corrupted file
+          shutil.rmtree(config.dataset_name + '/' + output_file) # remove the dir first to avoid writing a corrupted file
         extract_face_across_time(files, t_from, t_to, f, d, output_file + '.raw')
-        while len(glob.glob1('./', '*.raw')) >= 8: # do not spawn more than 8 processes
+        while len(glob.glob1('./', '*.raw')) >= config.n_processes:
           continue
           sleep(1)
-        p = Process(target = convert_to_idx2, args = (output_file + '.raw', dataset_name, long_field_name, dimensions))
+        p = Process(target = convert_to_idx2, args = (output_file + '.raw', config.dataset_name, long_field_name, dimensions))
         p.start()
