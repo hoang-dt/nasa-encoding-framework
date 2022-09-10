5898b2a091c7e4dd2bfe16e11e33567dc1e3f8d0
duong <hoangthaiduong@gmail.com>
2022-09-10 16:36:19 -0600

    Fix some more issues

--- conversion_scripts/extract-all_llc4320.py
+++ conversion_scripts/extract-all_llc4320.py
@@ -18,18 +18,18 @@ s = 4 # float322 = 4 bytes
 dfx = [nx    , nx    , nx, nx * 3, nx * 3] # x dimension of the 5 faces
 dfy = [ny * 3, ny * 3, ny, ny    , ny    ] # y dimension of the 5 faces
 nfaces = 5
-ndepths = 90
+ndepths = 1
 ntimes = len(files)
 
 for t in range(0, ntimes):
-  with open(files[t], "rb") as f:
+  with open(files[t], "rb") as F:
     for d in range(0, ndepths):
       skip_bytes = d * nx * ny * 13 * s # skip to the depth
       for f in range(0, nfaces):
         skip_bytes += dfx[f] * dfy[f] * s # skip to the face
         face_bytes = s * dfx[f] * dfy[f]
-        f.seek(skip_bytes)
-        buf = f.read(face_bytes)
+        F.seek(skip_bytes)
+        buf = F.read(face_bytes)
         dt = np.dtype('>')
         np_array_be = np.frombuffer(buf, dtype = dt)
         np_array_le = np_array_be.byteswap()
