ef618427a1ec139a420a050ddce6cf68fcc28b6f
duong <hoangthaiduong@gmail.com>
2022-09-10 23:41:49 -0600

    Rotate face 3 and 4

--- conversion_scripts/extract-all_llc4320.py
+++ conversion_scripts/extract-all_llc4320.py
@@ -33,6 +33,8 @@ for t in range(0, ntimes):
         dt = np.dtype('>f')
         np_array_be = np.frombuffer(buf, dtype = dt)
         np_array_le = np_array_be.byteswap()
+        if f > 2:
+          np_array_le = np.rot90(le)
         output = field + '-time-' + repr(t) + '-depth-' + repr(d) + '-face-' + repr(f) + '.raw'
         with open(output, 'w+b') as g:            
           g.write(np_array_le)    
